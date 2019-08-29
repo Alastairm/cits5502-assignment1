e41c46eabe89875aa2cdb25d01c211bf5c71de86
Alastair Mory <alastair.mory@gmail.com>
2019-08-29 13:13:57 +0800

    Started creating python plots
    * Cleaned up data.csv
    * Created basic histogram of plots
    * Added pip requirements file

--- py/plots.py
+++ py/plots.py
@@ -1,4 +1,31 @@
-import csv
+#!../env/bin/python3
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
-with open('../data.csv', newline='') as csvfile:
-    reader = csv.reader(csvfile)
+data = pd.read_csv('./data.csv')
+
+x = data['Week'].values
+hb = data['HB'].values
+hs = data['HS'].values
+eb = data['EB'].values
+es = data['ES'].values
+total = data['Total'].values
+
+
+def stacked_histogram():
+    plt.figure()
+
+    p1 = plt.bar(x, es)
+    p2 = plt.bar(x, eb, bottom=es)
+    p3 = plt.bar(x, hs, bottom=es+eb)
+    p4 = plt.bar(x, hb, bottom=es+eb+hs)
+
+    plt.title('Scores by group and gender')
+    plt.ylabel('Defects')
+    plt.xlabel('Week')
+    plt.xticks([5,10,15,20])
+
+    plt.show()
+
+stacked_histogram()
